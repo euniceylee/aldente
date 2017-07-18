8a05653a5b190c92f23943a24a0ed9f00b99e37e
pam- <pamela.assogba@voxmedia.com>
2017-07-18 09:58:53 -0400

    fixing indentation

--- get.py
+++ get.py
@@ -20,39 +20,37 @@ def job():
 
 
   # open premade card_names main file in append mode (a+) to keep track of card names
-  card_names = open("./card_names.txt", "a+")
+	card_names = open('./card_names.txt', 'a+')
 
 
   # open premade tasks file in append mode (a+) to use for printer
-  tasks = open("./tasks.txt", "a+")
+	tasks = open('./tasks.txt', 'a+')
 
   # iterate over each card check that card name is NOT in the card_names file
-  for obj in cards:
-      # if name is in the card_names file do nothing
-      if obj["id"] in open('card_names.txt').read():
-        print "already in the master list"
-        pass
-  	  else: # add name into task file and the card_names file
-        card_names.write(obj["id"]+"\n")
-        tasks.write("\n\n\n\n\n"+ obj["name"] + "\n\n\n\n\n:)")
-        print "yo i'm printing"
+	for obj in cards:
+		# if name is in the card_names file do nothing
+		if obj['id'] in open('card_names.txt').read():
+			print 'already in the master list'
+			pass
+		else: # add name into task file and the card_names file
+			card_names.write(obj['id']+'\n')
+			tasks.write('\n\n\n\n\n'+ obj['name'] + '\n\n\n\n\n:)')
+			print "yo i'm printing"
 
 
   # print task file if there's content
-
-  if os.stat("tasks.txt").st_size > 0:
-    call(["lpr","-o","fit-to-page", "tasks.txt"])
+	if os.stat('tasks.txt').st_size > 0:
+		call(['lpr','-o','fit-to-page', 'tasks.txt'])
 
 
   # clear task file
-
-  f= open('tasks.txt', 'r+')
-  f.truncate()
+	f= open('tasks.txt', 'r+')
+	f.truncate()
 
 # schedule job to run the code every 5 seconds
 schedule.every(2).seconds.do(job)
 
 # run task when app is ran via the python shell
 while True:
-  schedule.run_pending()
-  time.sleep(1)
+	schedule.run_pending()
+	time.sleep(1)
